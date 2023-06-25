7dea7a91002d9299e73ad84ecbcac86db04920d6
Ashour Abdal <ashurabdal89@gmail.com>
2023-06-25 18:59:47 +1000

    add API_key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -9,7 +9,7 @@ export interface FetchResponse<T> {
 const axiosInstance = axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: 'e53bba19d7914970889f528d70c8e06d',
+    key: '32578dd17efa49dbb3b411baee927d5d',
   },
 });
 
